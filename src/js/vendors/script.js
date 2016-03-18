cfb23f45fa3aeda947f1d70fa960568288c7c6ca
Silvio Rodrigues <silviorodrigues7@gmail.com>
2016-03-18 14:46:08 -0300

    Tradução do datePicker

--- src/js/vendors/script.js
+++ src/js/vendors/script.js
@@ -1,78 +1,100 @@
 $(document).ready(function(){
 
-	//ICON ATOM
-	$('[data-svgicon]').shapeSvgConvert();
+    //ICON ATOM
+    $('[data-svgicon]').shapeSvgConvert();
 
 
-	// MENU ATOM
-	$('.geo-menu__button').click(function( e ){
-		var gm = this.id,
-		gmFor = $('[data-geo-for="' + gm + '"]'),
-		gmId = $('#' + gm),
-		gmTop = gmId.offset().top,
-		gmLeft = gmId.offset().left;
+    // MENU ATOM
+    $('.geo-menu__button').click(function( e ){
+        var gm = this.id,
+        gmFor = $('[data-geo-for="' + gm + '"]'),
+        gmId = $('#' + gm),
+        gmTop = gmId.offset().top,
+        gmLeft = gmId.offset().left;
 
 
-		e.stopPropagation();
-		$('[data-geo-for]').hide();
-		gmFor.show();
+        e.stopPropagation();
+        $('[data-geo-for]').hide();
+        gmFor.show();
 
-		if(gmFor.hasClass('geo-menu--bottom-right')) {
-			gmFor.css({top: gmTop + gmId.height(), left: gmLeft + gmId.width()});
-		}else if(gmFor.hasClass('geo-menu--bottom-left')) {
-			gmFor.css({top: gmTop + gmId.height(), left: gmLeft - gmFor.width()});
-		}else if(gmFor.hasClass('geo-menu--top-right')) {
-			gmFor.css({top: gmTop - gmId.height()  - gmFor.height(), left: gmLeft + gmId.width()});
-		}else if(gmFor.hasClass('geo-menu--top-left')) {
-			gmFor.css({top: gmTop - gmId.height()  - gmFor.height(), left: gmLeft - gmFor.width()});
-		}
+        if(gmFor.hasClass('geo-menu--bottom-right')) {
+            gmFor.css({top: gmTop + gmId.height(), left: gmLeft + gmId.width()});
+        }else if(gmFor.hasClass('geo-menu--bottom-left')) {
+            gmFor.css({top: gmTop + gmId.height(), left: gmLeft - gmFor.width()});
+        }else if(gmFor.hasClass('geo-menu--top-right')) {
+            gmFor.css({top: gmTop - gmId.height()  - gmFor.height(), left: gmLeft + gmId.width()});
+        }else if(gmFor.hasClass('geo-menu--top-left')) {
+            gmFor.css({top: gmTop - gmId.height()  - gmFor.height(), left: gmLeft - gmFor.width()});
+        }
 
-	});
-	$('body').click(function(){
-		$('[data-geo-for]').hide();
-	});
-	
-	window.document.onkeydown = function (e) {
-		if (!e) e = event;
-		if (e.keyCode == 27) {
-			$('[data-geo-for]').hide();
-		}
-	}
+    });
+    $('body').click(function(){
+        $('[data-geo-for]').hide();
+    });
+    
+    window.document.onkeydown = function (e) {
+        if (!e) e = event;
+        if (e.keyCode == 27) {
+            $('[data-geo-for]').hide();
+        }
+    }
 
-	//MENU REDUCE
-	$('[data-logo="reduced"]').hide();
-	$('.geo-nav__footer').click( function(){
-		if ($('.geo-nav').hasClass('geo-nav--reduced')) {
-			$('.geo-nav').removeClass('geo-nav--reduced');
-			$('.geo-header').removeClass('geo-header--reduced');
-			$('.geo-content').removeClass('geo-content--reduced');
-			$('[data-logo="expand"]').show();
-			$('[data-logo="reduced"]').hide();
-		} else {
-			$('.geo-nav').addClass('geo-nav--reduced');
-			$('.geo-header').addClass('geo-header--reduced');
-			$('.geo-content').addClass('geo-content--reduced');
-			$('[data-logo="expand"]').hide();
-			$('[data-logo="reduced"]').show();
-		}
-	});
+    //MENU REDUCE
+    $('[data-logo="reduced"]').hide();
+    $('.geo-nav__footer').click( function(){
+        if ($('.geo-nav').hasClass('geo-nav--reduced')) {
+            $('.geo-nav').removeClass('geo-nav--reduced');
+            $('.geo-header').removeClass('geo-header--reduced');
+            $('.geo-content').removeClass('geo-content--reduced');
+            $('[data-logo="expand"]').show();
+            $('[data-logo="reduced"]').hide();
+        } else {
+            $('.geo-nav').addClass('geo-nav--reduced');
+            $('.geo-header').addClass('geo-header--reduced');
+            $('.geo-content').addClass('geo-content--reduced');
+            $('[data-logo="expand"]').hide();
+            $('[data-logo="reduced"]').show();
+        }
+    });
 
-	// NAV MOBILE
-	$('.geo-mobile__toggle').click( function(){
-		$('.geo-nav').addClass('geo-nav--active');
-		$('.geo-nav__overlay').addClass('active');
-	});
+    // NAV MOBILE
+    $('.geo-mobile__toggle').click( function(){
+        $('.geo-nav').addClass('geo-nav--active');
+        $('.geo-nav__overlay').addClass('active');
+    });
 
-	$('.geo-nav__overlay').click( function(){
-		$('.geo-nav').removeClass('geo-nav--active');
-		$('.geo-nav__overlay').removeClass('active');
-	});
+    $('.geo-nav__overlay').click( function(){
+        $('.geo-nav').removeClass('geo-nav--active');
+        $('.geo-nav__overlay').removeClass('active');
+    });
 
-	//MESSAGES REDUCED
-	
-	$('.geo-chat').on('click', '.geo-message__header', function() {
-		$(this).parent('.geo-chat__message').toggleClass('geo-chat__message--reduced');  
-	});
+    //MESSAGES REDUCED
+    
+    $('.geo-chat').on('click', '.geo-message__header', function() {
+        $(this).parent('.geo-chat__message').toggleClass('geo-chat__message--reduced');  
+    });
+
+    $.datepicker.regional['pt-BR'] = {
+        closeText: 'Fechar',
+        prevText: '&#x3c;Anterior',
+        nextText: 'Pr&oacute;ximo&#x3e;',
+        currentText: 'Hoje',
+        monthNames: ['Janeiro','Fevereiro','Mar\u00e7o','Abril','Maio','Junho', 'Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+        monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun', 'Jul','Ago','Set','Out','Nov','Dez'],
+        dayNames: ['Domingo','Segunda-feira','Ter&ccedil;a-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sabado'],
+        dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+        dayNamesMin: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+        weekHeader: 'Sm',
+        dateFormat: 'dd/mm/yy',
+        firstDay: 0,
+        isRTL: false,
+        showMonthAfterYear: false,
+        yearSuffix: ''
+    };
+
+    $.datepicker.setDefaults($.datepicker.regional['pt-BR']);
 
 });
+
+
 
