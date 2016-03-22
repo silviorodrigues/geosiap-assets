e7f6f64b75ad3c33fd43c532a8db81fc3539b894
Silvio Rodrigues <silviorodrigues7@gmail.com>
2016-03-22 11:56:47 -0300

    Input after clickable

--- src/js/vendors/script.js
+++ src/js/vendors/script.js
@@ -74,6 +74,11 @@ $(document).ready(function(){
         $(this).parent('.geo-chat__message').toggleClass('geo-chat__message--reduced');  
     });
 
+    //INPUT FOCUS
+    $('.geo__input--12c, .geo__input--9c, .geo__input--6c, .geo__input--3c, .geo__input--1c').click(function(){
+        $(this).children('div').children('input').focus();
+    });
+
     $.datepicker.regional['pt-BR'] = {
         closeText: 'Fechar',
         prevText: '&#x3c;Anterior',
