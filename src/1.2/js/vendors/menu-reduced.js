d0e638b18f82fb3fb256f2d28c2912099e0596fd
Silvio Rodrigues <silviorodrigues7@gmail.com>
2016-05-20 11:11:49 -0300

    Função para container reduced

--- src/1.2/js/vendors/menu-reduced.js
+++ src/1.2/js/vendors/menu-reduced.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-
     $('[data-logo="reduced"]').hide();
     $('.geo-nav__footer').click( function(){
         if ($('.geo-nav').hasClass('geo-nav--reduced')) {
             $('.geo-nav').removeClass('geo-nav--reduced');
+            $('.geo-framework__content').removeClass('reduced');
             $('[data-logo="expand"]').show();
             $('[data-logo="reduced"]').hide();
         } else {
             $('.geo-nav').addClass('geo-nav--reduced');
+            $('.geo-framework__content').addClass('reduced');
             $('[data-logo="expand"]').hide();
             $('[data-logo="reduced"]').show();
         }
