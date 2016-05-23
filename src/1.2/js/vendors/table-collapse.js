baf273f40a00ed199a13cdbd20416611f58f0176
Silvio Rodrigues <silviorodrigues7@gmail.com>
2016-05-23 10:32:27 -0300

    Ajustado collapse da Table

--- src/1.2/js/vendors/table-collapse.js
+++ src/1.2/js/vendors/table-collapse.js
@@ -1,8 +1,4 @@
-$(document).ready(function(){
-
-    $('[data-collapse]').on('click', 'tr', function() {
-      $(this).find('.geo-arrow').toggleClass('geo-arrow--down');
-      $(this).next().toggleClass('active');
-    });
-
+$(document).on('click', '[data-collapse] tr', function() {
+  $(this).find('.geo-arrow').toggleClass('geo-arrow--down');
+  $(this).next().toggleClass('active');
 });
